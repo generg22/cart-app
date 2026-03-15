7c1c04aaf6ba4501daefe9930520acd6b7233bbf
Genesis <genesishipogrifo22@gmail.com>
2026-03-15 17:20:35 -0400

    cambios realizados

--- src/app/components/cart-app.ts
+++ src/app/components/cart-app.ts
@@ -2,22 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { Product } from '../models/product';
 import { Catalog } from './catalog/catalog';
-import { Cart } from './cart/cart';
 import { CartItem } from '../models/cartItem';
 import { Navbar } from './navbar/navbar';
+import { CartModal } from './cart-modal/cart-modal';
 
 @Component({
   selector: 'cart-app',
   standalone: true,
-  imports: [Catalog, Cart, Navbar],
+  imports: [Catalog, CartModal, Navbar],
   templateUrl: './cart-app.html',
 })
 export class CartApp implements OnInit {
   products: Product[] = [];
 
   items: CartItem[] = [];
-
-  total: number = 0;
 
   showCart: boolean = false;
 
@@ -26,7 +24,6 @@ export class CartApp implements OnInit {
   ngOnInit(): void {
     this.products = this.service.findAll();
     this.items = JSON.parse(sessionStorage.getItem('cart') || '[]');
-    this.calculateTotal();
   }
 
   onAddCart(product: Product): void {
@@ -42,25 +39,20 @@ export class CartApp implements OnInit {
     } else {
       this.items = [...this.items, { product: { ...product }, quantity: 1 }];
     }
-    this.calculateTotal();
-    this.saveSession();
   }
 
   onDeleteCart(id: number): void {
     this.items = this.items.filter((item) => item.product.id !== id);
-    this.calculateTotal();
-    this.saveSession();
+
+    if (this.items.length == 0) {
+      sessionStorage.removeItem('cart');
+      sessionStorage.clear();
+    }
+
   }
 
-  calculateTotal(): void {
-    this.total = this.items.reduce((acc, item) => acc + item.product.price * item.quantity, 0);
-  }
-
-  saveSession(): void {
-    sessionStorage.setItem('cart', JSON.stringify(this.items));
-  }
-
-  openCart(): void {
+  openCloseCart(): void {
     this.showCart = !this.showCart;
   }
+
 }
--- src/app/components/cart/cart.ts
+++ src/app/components/cart/cart.ts
@@ -1,19 +1,35 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { CartItem } from '../../models/cartItem';
 
 @Component({
   selector: 'cart',
   imports: [],
-  templateUrl: './cart.html'
+  templateUrl: './cart.html',
 })
-export class Cart {
+export class Cart implements OnChanges {
+  @Input() items: CartItem[] = [];
 
-  @Input() items : CartItem[] = [];
-  @Input() total : number = 0;
-  
+  total = 0;
+
   @Output() idProductEventEmitter = new EventEmitter();
 
-  onDeleteCart(id: number){
+  ngOnChanges(changes: SimpleChanges): void {
+    let itemsChanges = changes['items'];
+    
+    this.calculateTotal();
+    this.saveSession();
+    
+  }
+
+  onDeleteCart(id: number) {
     this.idProductEventEmitter.emit(id);
+  }
+
+  calculateTotal(): void {
+    this.total = this.items.reduce((acc, item) => acc + item.product.price * item.quantity, 0);
+  }
+
+  saveSession(): void {
+    sessionStorage.setItem('cart', JSON.stringify(this.items));
   }
 }
